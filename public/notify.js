d9c203374d71614be579636039df3b8472564bbf
Oluwakeye John <bjohnoluwakeye@gmail.com>
2020-06-23 15:28:51 +0100

    added notification error

--- public/notify.js
+++ public/notify.js
@@ -1,38 +1,42 @@
 function notifyMe(title, text) {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
+  try {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
 
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification(title, {
-      body: text,
-    });
-  }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification(title, {
+        body: text,
+      });
+    }
 
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification(title, {
-          body: text,
-        });
-      }
-    });
-  } else {
-    Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification(title, {
-          body: text,
-        });
-      }
-    });
-  }
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification(title, {
+            body: text,
+          });
+        }
+      });
+    } else {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification(title, {
+            body: text,
+          });
+        }
+      });
+    }
 
-  // At last, if the user has denied notifications, and you
-  // want to be respectful there is no need to bother them any more.
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
+  } catch {
+    console.log("Error showing notifications");
+  }
 }
